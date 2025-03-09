88714b3714baa93ef89103fab08fb59d08016f6a
Stephan Raab <sarcartist@gmail.com>
2025-03-08 22:52:07 -0500

    not sure why I was trying to do this recursively...need to re-write and test

--- src/day1/BinarySearchList.ts
+++ src/day1/BinarySearchList.ts
@@ -1,3 +1,27 @@
 export default function bs_list(haystack: number[], needle: number): boolean {
+  // get the length and divide it by two
+  // check if value at the middle === needle, is greater or smaller
+  // update start and end indices
+  //
 
+  function search (arr: number[], lo: number, hi: number, needle: number) {
+    let middle = Math.floor(lo + (hi - lo)/2)
+  
+    do {
+     if (arr[middle] === needle){
+      return true;
+    } else if (needle > arr[middle]){
+      lo = middle +1;
+      search(arr, lo, hi, needle);
+    } else {
+      hi = middle;
+      search(arr, lo, hi, needle);
+    }
+    }
+    while (lo < hi);
+
+    return false;
+  }
+  
+  search(haystack, 0, haystack.length, needle);
 }
